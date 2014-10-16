92d9083ef113b5f8a74c47ebc0ae5e3109558b2c
thehobn <thehobn@gmail.com>
2014-10-15 18:12:34 -0700

    more documentation; show discount as negative

--- Bobcar.java
+++ Bobcar.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 /**
  * 
- * 09/24/14 CSE020 UCM Project 1
- * @author Huimin Zhang and Johnson Mei
- * @version 1
+ * 10/15/14 CSE020 UCM Project 1
+ * @author Huimin Zhang
+ * @author Johnson Mei
+ * @version 1.1
  *
  */
 public class Bobcar {
 	static Scanner input = new Scanner(System.in);
+
+	//Arrays for easy access to car names and prices
 	static String[] carList = {"Economy", "Compact", "Standard"};
 	static int[] costList = {22, 55, 100};
+
+	//This variable can be easily modified to change currency (don't forget to change cost)
 	static char currency = '$';
+
+	//Variables to store user choices
 	static int carChosen;
 	static int rentalDays;
 	static boolean isMember;
 	static boolean wantsPackage;
+
+	//The price variable is changed after each stage of the calculation
 	static int price;
 
 	public static void main(String[] args) {
@@ -25,22 +34,32 @@ public class Bobcar {
 			wantsPackage = askPackage();
 		price = calculatePrice();
 	}
+
 	public static int calculatePrice(){
+		/*
+		 * Base price calculation
+		 */
+		
 		//Show user the chosen car and its price
 		System.out.println("Cost of chosen car, " + carList[carChosen] + ", " +
 				"is" + ' ' + currency + costList[carChosen] + " per day. ");
 
 		//Show user the rental period
 		System.out.println("Rental period is " + rentalDays);
+
 		//Handle plurality
 		if (rentalDays == 1)
 			System.out.println(" day.");
 		else
 			System.out.println(" days.");
-		
+
 		//Set price to the base price and print
 		price = costList[carChosen] * rentalDays;
 		System.out.println("Base price is " + currency + price);
+		
+		/*
+		 * Club discount and premium package cost
+		 */
 		
 		//Apply club discount if applicable
 		if (isMember == true) {
@@ -48,13 +67,13 @@ public class Bobcar {
 			System.out.println("Club discount is " + currency + costList[carChosen] * rentalDays / 5);
 
 		}
-		
+
 		//Add package price if chosen
 		if (wantsPackage == true) {
 			price *= 1.2;
 			System.out.println("Package cost is " + currency + price * 0.2);
 		}
-		
+
 		//Final price
 		System.out.println("Final price is " + currency + price);
 		return price;
@@ -63,13 +82,14 @@ public class Bobcar {
 	public static int askCar() {
 		//Print choices
 		System.out.println("Select from the following rental cars: ");
+		//Iterate through array of cars available and list them
 		for (int i=0;i<carList.length;i++) {
 			System.out.println("Press" + ' ' + i + ' ' + "for" + ' ' + carList[i]);
 		}
-		
+
 		//Take input
 		int tempCarChosen = input.nextInt();
-		
+
 		//Check input validity
 		if (tempCarChosen >= 0 && tempCarChosen < carList.length)
 			return tempCarChosen;
@@ -85,14 +105,14 @@ public class Bobcar {
 	}
 
 	public static boolean askMember() {
-		//List choices
+		//Prompt user
 		System.out.println("Are you a club member?");
 		System.out.println("Press 1 for yes");
 		System.out.println("Press 0 for no");
-		
+
 		//Take input
 		int tempIsMember = input.nextInt();
-		
+
 		//Parse input and check validity
 		if (tempIsMember == 1)
 			return true;
@@ -102,18 +122,17 @@ public class Bobcar {
 			System.out.println("Invalid Input!");
 			return askMember();
 		}
-
 	}
 
 	public static boolean askPackage() {
-		//List Choices
+		//Prompt user
 		System.out.println("Do you want the Platinum Exclusive Package?");
 		System.out.println("Press 1 for yes");
 		System.out.println("Press 0 for no");
-		
+
 		//Take input
 		int tempWantsPackage = input.nextInt();
-		
+
 		//Parse input and check validity
 		if (tempWantsPackage == 1)
 			return true;
